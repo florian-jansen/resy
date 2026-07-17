8c2144fc1f8db6c8a9a806018191b738d6f04df6
markus1bauer <markus1.bauer@tum.de>
2026-07-17 17:30:14 +0200

    Update test-data-doc.R
    Solve failed tests

--- tests/testthat/test-data-doc.R
+++ tests/testthat/test-data-doc.R
@@ -1,44 +1,50 @@
 test_that("dunes_bohn_500mbuffer_epsg25832 dataset loads correctly", {
   # Load the dataset
-  data("dunes_bohn_500mbuffer_epsg25832", package = "RESY", envir = environment())
+  utils::data(
+    "dunes_bohn_500mbuffer_epsg25832", package = "RESY", envir = environment()
+    )
   
   # Check if the dataset exists
-  expect_true(exists("dunes_bohn_500mbuffer_epsg25832", envir = environment()))
+  expect_true(exists("bohn", envir = environment()))
   
   # Check if it is an sf object
-  expect_s3_class(dunes_bohn_500mbuffer_epsg25832, "sf")
+  expect_s3_class(bohn, "sf")
   
   # Check CRS
-  expect_equal(st_crs(dunes_bohn_500mbuffer_epsg25832)$epsg, 25832)
+  expect_equal(st_crs(bohn)$epsg, 25832)
   
   # Check if it is not empty
-  expect_true(nrow(dunes_bohn_500mbuffer_epsg25832) > 0)
+  expect_true(nrow(bohn) > 0)
 })
 
 ---
   
   test_that("coastline_regions_epsg25832 dataset loads correctly", {
     # Load the dataset
-    data("coastline_regions_epsg25832", package = "RESY", envir = environment())
+    utils::data(
+      "coastline_regions_epsg25832", package = "RESY", envir = environment()
+      )
     
     # Check if the dataset exists
-    expect_true(exists("coastline_regions_epsg25832", envir = environment()))
+    expect_true(exists("co", envir = environment()))
     
     # Check if it is an sf object
-    expect_s3_class(coastline_regions_epsg25832, "sf")
+    expect_s3_class(co, "sf")
     
     # Check CRS
-    expect_equal(st_crs(coastline_regions_epsg25832)$epsg, 25832)
+    expect_equal(st_crs(co)$epsg, 25832)
     
     # Check if it is not empty
-    expect_true(nrow(coastline_regions_epsg25832) > 0)
+    expect_true(nrow(co) > 0)
   })
 
 ---
   
   test_that("ecoregions2017_epsg25832 dataset loads correctly", {
     # Load the dataset
-    data("ecoregions2017_epsg25832", package = "RESY", envir = environment())
+    utils::data(
+      "ecoregions2017_epsg25832", package = "RESY", envir = environment()
+      )
     
     # Check if the dataset exists
     expect_true(exists("ecoregions2017_epsg25832", envir = environment()))
@@ -56,7 +62,9 @@ test_that("dunes_bohn_500mbuffer_epsg25832 dataset loads correctly", {
 ---
   test_that("europe_resolution_1_epsg25832 dataset loads correctly", {
     # Load the dataset
-    data("europe_resolution_1_epsg25832", package = "RESY", envir = environment())
+    utils::data(
+      "europe_resolution_1_epsg25832", package = "RESY", envir = environment()
+      )
     
     # Check if the dataset exists
     expect_true(exists("europe_resolution_1_epsg25832", envir = environment()))
@@ -74,7 +82,9 @@ test_that("dunes_bohn_500mbuffer_epsg25832 dataset loads correctly", {
 ---
   test_that("europe_resolution_60_epsg25832 dataset loads correctly", {
     # Load the dataset
-    data("europe_resolution_60_epsg25832", package = "RESY", envir = environment())
+    utils::data(
+      "europe_resolution_60_epsg25832", package = "RESY", envir = environment()
+      )
     
     # Check if the dataset exists
     expect_true(exists("europe_resolution_60_epsg25832", envir = environment()))
